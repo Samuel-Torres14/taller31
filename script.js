614d36e10c12a094d66a7201020ce34f37d2baef
samuel torres <est.samuelj.torres@unimilitar.edu.co>
2026-05-08 09:51:18 -0500

    Implementación de la ventana de recorte configurable

--- script.js
+++ script.js
@@ -2,3 +2,24 @@ const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
 console.log("Canvas conectado correctamente");
+
+let xMin = 150;
+let yMin = 100;
+let xMax = 550;
+let yMax = 350;
+
+function drawViewport(){
+
+    ctx.strokeStyle = "blue";
+
+    ctx.strokeRect(
+        xMin,
+        yMin,
+        xMax - xMin,
+        yMax - yMin
+    );
+}
+
+ctx.clearRect(0,0,canvas.width,canvas.height);
+
+drawViewport();
