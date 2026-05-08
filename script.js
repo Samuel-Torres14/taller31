ef2f51fc2e4aef71aee44cf0ab1819d50e415eec
samuel torres <est.samuelj.torres@unimilitar.edu.co>
2026-05-08 09:52:48 -0500

    Implementación de función parametrizada para trazado de líneas

--- script.js
+++ script.js
@@ -23,3 +23,22 @@ function drawViewport(){
 ctx.clearRect(0,0,canvas.width,canvas.height);
 
 drawViewport();
+
+function drawLine(x1,y1,x2,y2,color="black",grosor=2){
+
+    ctx.beginPath();
+
+    ctx.moveTo(x1,y1);
+
+    ctx.lineTo(x2,y2);
+
+    ctx.strokeStyle = color;
+
+    ctx.lineWidth = grosor;
+
+    ctx.stroke();
+}
+
+// Línea de prueba
+
+drawLine(50,50,700,400,"gray",2);
